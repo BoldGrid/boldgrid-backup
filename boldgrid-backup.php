419de3e51c85b049b7a769f1452d18d910ff9f1d
BradM <bradm@inmotionhosting.com>
2017-10-17 08:36:04 -0400

    Updating version numbers

--- boldgrid-backup.php
+++ boldgrid-backup.php
@@ -15,7 +15,7 @@
  *          Plugin Name: BoldGrid Backup
  *          Plugin URI: https://www.boldgrid.com/boldgrid-backup/
  *          Description: The BoldGrid Backup plugin.
- *          Version: 1.5.1
+ *          Version: 1.5.2
  *          Author: BoldGrid.com
  *          Author URI: https://www.boldgrid.com/
  *          License: GPL-2.0+
