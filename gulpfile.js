77dcd61cf0b968fa0fcdaa1194f26093ea442053
Joe Cartonia <joe@css-solutions.com>
2019-01-11 10:13:43 -0500

    Updated readme files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,7 @@
-var gulp = require( 'gulp' ),
-	readme = require( 'gulp-readme-to-markdown' );
-
-gulp.task( 'readme', function() {
-	gulp
-		.src( [ 'readme.txt' ] )
-		.pipe( readme() )
-		.pipe( gulp.dest( '.' ) );
-} );
+var gulp = require( 'gulp' );
 
 gulp.task( 'build', function() {
-	gulp.src( [ 'node_modules/clipboard/dist/clipboard.min.js' ] ).pipe( gulp.dest( './build' ) );
+	gulp.src( [ 'node_modules/clipboard/dist/clipboard.min.js' ] ).pipe( gulp.dest( './build/' ) );
 } );
 
-gulp.task( 'default', [ 'readme', 'build' ] );
+gulp.task( 'default', [ 'build' ] );
