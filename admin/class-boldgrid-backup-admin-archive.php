f9db926f86e1729cd09b41485cf491f8be84e705
Joe Cartonia <joe@css-solutions.com>
2019-01-30 15:24:39 -0500

    Added file_md5 to restore-info file.

--- admin/class-boldgrid-backup-admin-archive.php
+++ admin/class-boldgrid-backup-admin-archive.php
@@ -542,6 +542,7 @@ class Boldgrid_Backup_Admin_Archive {
 				'archive_key' => $archive_key,
 				'cron_secret' => $cron_secret,
 				'filepath'    => $archive_filepath,
+				'file_md5'    => $info['file_md5'],
 				'siteurl'     => $siteurl,
 				'restore_cmd' => $this->core->cron->get_cron_command() . ' "' .
 					dirname( __DIR__ ) . '/boldgrid-backup-cron.php" mode=restore siteurl=' .
