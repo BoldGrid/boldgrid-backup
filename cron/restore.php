4287d489ba6df14e23a351b64108ee5a753fcae5
Joe Cartonia <joe@css-solutions.com>
2019-02-08 15:31:23 -0500

    Added PHP version check for ERP.

--- cron/restore.php
+++ cron/restore.php
@@ -13,7 +13,18 @@
  * @copyright  BoldGrid
  * @version    $Id$
  * @author     BoldGrid <support@boldgrid.com>
+ *
+ * phpcs:disable WordPress.VIP,WordPress.XSS.EscapeOutput
  */
+
+// Ensure PHP compatibility.
+$php_min_version = '5.4';
+
+if ( version_compare( PHP_VERSION, $php_min_version, '<' ) ) {
+	echo 'Error: Incompatible PHP version "' . PHP_VERSION . '".  This utility requires "' .
+		$php_min_version . '" or higher.' . PHP_EOL;
+	exit( 1 );
+}
 
 // Run the restoration.
 require __DIR__ . '/class-boldgrid-backup-restore.php';
