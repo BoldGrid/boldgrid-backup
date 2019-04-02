7a7316165fb4cc6355c452f664c31eb170ba2344
Joe Cartonia <joe@css-solutions.com>
2019-04-02 14:41:23 -0400

    Updated version to 1.9.1.

--- boldgrid-backup.php
+++ boldgrid-backup.php
@@ -16,7 +16,7 @@
  *          Plugin Name: BoldGrid Backup
  *          Plugin URI: https://www.boldgrid.com/boldgrid-backup/
  *          Description: BoldGrid Backup provides WordPress backup and restoration with update protection.
- *          Version: 1.9.0
+ *          Version: 1.9.1
  *          Author: BoldGrid
  *          Author URI: https://www.boldgrid.com/
  *          License: GPL-2.0+
