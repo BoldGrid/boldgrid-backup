4ea258daa6526a8e92a1cf765ad3629e59e4b6ca
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2020-05-28 09:43:54 -0400

    Avoid fatal on library issues. Fixes #283

--- admin/class-boldgrid-backup-admin-support.php
+++ admin/class-boldgrid-backup-admin-support.php
@@ -48,13 +48,17 @@ class Boldgrid_Backup_Admin_Support {
 	}
 
 	/**
-	 * Deactivate and show an error.
+	 * Add an admin notice.
+	 *
+	 * This method use to be "deactivate". Users only have 1 chance to see the message we're showing
+	 * if we deactivate the plugin. If we instead just show an admin message, the user has more than
+	 * once chance to see the notice and take action to resolve the issue.
 	 *
 	 * @since 1.7.0
 	 *
 	 * @param string $error Error message.
 	 */
-	public function deactivate( $error ) {
+	public function add_admin_notice( $error ) {
 		add_action(
 			'admin_notices', function () use ( $error ) {
 				$allowed_html = [
@@ -62,15 +66,32 @@ class Boldgrid_Backup_Admin_Support {
 					'strong' => [],
 					'br'     => [],
 					'em'     => [],
+					'pre'    => [],
+					'a'      => [
+						'href'   => [],
+						'target' => [],
+					],
 				];
 
 				$error = '<p>' . sprintf(
 					// translators: 1: HTML opening strong tags, 2: HTML closing strong tag, 3: Plugin title.
-					__( '%1$s%3$s%2$s has been deactivated due to the following error:', 'boldgrid-backup' ),
+					__( '%1$s%3$s%2$s is unable to load due to the following error:', 'boldgrid-backup' ),
 					'<strong>',
 					'</strong>',
 					BOLDGRID_BACKUP_TITLE
 				) . '<br /><br />' . $error . '</p>';
+
+				// Inform the user how to get help.
+				$error .= '<p>' . sprintf(
+					// translators: 1 Plugin title, 2 opening anchor tag linking to plugin page, 3 url to plugin page, 4 closing anchor tag.
+					__( 'Please deactivate / reactivate Total Upkeep as this often resolves issues. If you are installing %1$s from .zip, ensure you downloaded it from %2$s%3$s%4$s. For additional help, please post a question in the %5$sWordPress Support Forums.%4$s', 'boldgrid-backup' ),
+					BOLDGRID_BACKUP_TITLE,
+					'<a href="https://wordpress.org/plugins/boldgrid-backup/" target="_blank">',
+					'https://wordpress.org/plugins/boldgrid-backup/',
+					'</a>',
+					'<a href="https://wordpress.org/support/plugin/boldgrid-backup/#new-topic-0" target="_blank">'
+				) . '</p>';
+
 
 				// Echo our admin notice. Hide the "plugin activated" notice.
 				echo '
@@ -79,12 +100,6 @@ class Boldgrid_Backup_Admin_Support {
 					.updated.notice { display: none; }
 				</style>
 			';
-			}
-		);
-
-		add_action(
-			'admin_init', function() {
-				deactivate_plugins( 'boldgrid-backup/boldgrid-backup.php', true );
 			}
 		);
 	}
@@ -115,6 +130,21 @@ class Boldgrid_Backup_Admin_Support {
 	}
 
 	/**
+	 * Verify appropriate library is available.
+	 *
+	 * This is a very basic test. It could be more exhaustive. However, a missing library is rare and
+	 * exhaustive tests are not needed.
+	 *
+	 * @since 1.13.5
+	 *
+	 * @return bool
+	 */
+	public function has_library() {
+		// This can be updated to the newest library classes to check for a more recent version.
+		return class_exists( 'Boldgrid\Library\Library\Usage\Notice' );
+	}
+
+	/**
 	 * Whether or not this version of the Backup Plugin is compatible with the premium extension.
 	 *
 	 * @since 1.11.3
@@ -129,6 +159,9 @@ class Boldgrid_Backup_Admin_Support {
 	/**
 	 * Run tests.
 	 *
+	 * These tests are triggered by the main class-boldgrid-backup.php file. If these tests fail, the
+	 * rest of the plugin will not load.
+	 *
 	 * @since 1.7.1
 	 *
 	 * @see has_compatible_php()
@@ -138,8 +171,13 @@ class Boldgrid_Backup_Admin_Support {
 	 * @return bool
 	 */
 	public function run_tests() {
+		// Utility method required in this method.
+		if ( ! class_exists( 'Boldgrid_Backup_Admin_Utility' ) ) {
+			require_once BOLDGRID_BACKUP_PATH . '/admin/class-boldgrid-backup-admin-utility.php';
+		}
+
 		if ( ! $this->has_compatible_php() ) {
-			$this->deactivate(
+			$this->add_admin_notice(
 				sprintf(
 					// Translators: 1: Current PHP version, 2: Minimum supported PHP version.
 					__(
@@ -155,12 +193,35 @@ class Boldgrid_Backup_Admin_Support {
 		}
 
 		if ( ! $this->has_composer_installed() ) {
-			$this->deactivate(
+			$this->add_admin_notice(
 				__(
 					'The vendor folder is missing. Please run "composer install", or contact your host for further assistance.',
 					'boldgrid-backup'
 				)
 			);
+
+			return false;
+		}
+
+		/*
+		 * Do a basic test and ensure we have access to the library.
+		 *
+		 * In theory, we should never have an issue with the library loading. This method should never
+		 * be needed, and any issues with the library should be troubleshooted and resolved. However,
+		 * we cannot have a library issue cause a fatal error, hence this check.
+		 *
+		 * Total Upkeep's library is only registered after activation, hence the is_active() check below.
+		 */
+		if ( Boldgrid_Backup_Admin_Utility::is_active() && ! $this->has_library() ) {
+			$boldgrid_settings = get_option( 'boldgrid_settings', array() );
+
+			$this->add_admin_notice( sprintf(
+				__(
+					'One or more library files are missing. Registered libraries: %1$s',
+					'boldgrid-backup'
+				),
+				! empty( $boldgrid_settings['library'] ) ? '<pre>' . print_r( $boldgrid_settings['library'], 1 ) . '</pre>' : __( 'None', 'boldgrid-backup' )
+			));
 
 			return false;
 		}
