29123e2fad59eb27f92b5bdc4faa46dfd36ea42c
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2020-11-24 10:20:06 -0500

    test 1

---
+++ tests/acceptance/FirstCest.php
@@ -0,0 +1,16 @@
+<?php 
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+
+        $I->see('Grow and Manage');
+    }
+}
