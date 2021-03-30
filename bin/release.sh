f1eef364c6f796621fa74d449298df5ff98a74f0
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2021-03-30 10:27:12 -0400

    Delete git folders. Fixes #455

--- bin/release.sh
+++ bin/release.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# Cleanup uneeded .gitignore files.
+# Cleanup uneeded git content.
 find . -name ".gitignore" -type f -delete
+echo "Finding and deleting .git folders."
+find . -name ".git" -type d -exec echo {} +
+find . -name ".git" -type d -exec rm -rf {} +
 
 # Create a tag in the Wordpress.org SVN repo when after your build succeeds via Travis.
 # https://github.com/BoldGrid/wordpress-tag-sync
