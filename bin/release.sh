226bad0a4526b28b06c01c0ff178ff5b095caa40
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2021-03-30 10:47:02 -0400

    Only delete git folders in vendor. Fixes #455

--- bin/release.sh
+++ bin/release.sh
@@ -3,8 +3,8 @@
 # Cleanup uneeded git content.
 find . -name ".gitignore" -type f -delete
 echo "Finding and deleting .git folders."
-find . -name ".git" -type d -exec echo {} +
-find . -name ".git" -type d -exec rm -rf {} +
+find vendor/. -name ".git" -type d -exec echo {} +
+find vendor/. -name ".git" -type d -exec rm -rf {} +
 
 # Create a tag in the Wordpress.org SVN repo when after your build succeeds via Travis.
 # https://github.com/BoldGrid/wordpress-tag-sync
