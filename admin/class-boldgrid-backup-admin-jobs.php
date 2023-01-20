2e4f70db3b18093d2dc5725f0aed8afb10c0cc57
SitesByYogi <90996605+SitesByYogi@users.noreply.github.com>
2023-01-20 09:31:40 -0500

    Update class-boldgrid-backup-admin-jobs.php

--- admin/class-boldgrid-backup-admin-jobs.php
+++ admin/class-boldgrid-backup-admin-jobs.php
@@ -226,15 +226,15 @@ class Boldgrid_Backup_Admin_Jobs {
 	}
 
 	/**
- 	* Fix stalled jobs.
- 	*
- 	* A stalled job is a job who's status has been set to 'running',
- 	* however it's been running longer than expected.
- 	*
- 	* For example, if a job is only supposed to take 1 minute, and it's
- 	* been running for 3 hours, it's stalled. Most likely the process was
- 	* either killed and or had a fatal error.
- 	*/
+	* Fix stalled jobs.
+	*
+	* A stalled job is a job who's status has been set to 'running',
+	* however it's been running longer than expected.
+	*
+	* For example, if a job is only supposed to take 1 minute, and it's
+	* been running for 3 hours, it's stalled. Most likely the process was
+	* either killed and or had a fatal error.
+	*/
 	private function maybe_fixed_stalled() {
 		$this->set_jobs();
 
@@ -242,27 +242,27 @@ class Boldgrid_Backup_Admin_Jobs {
 		$have_changes = false;
 
 		foreach ( $this->jobs as &$job ) {
-		// If the job never started, then it can't be stalled. Move on to the next job.
+			// If the job never started, then it can't be stalled. Move on to the next job.
 			if ( empty( $job['start_time'] ) ) {
 				continue;
-	}
+			}
 
 			// Determine whether or not this job is stalled.
 			$time_limit = HOUR_IN_SECONDS;
 			$duration   = time() - $job['start_time'];
 			$is_stalled = $duration > $time_limit;
-	  
+
 			if ( $is_stalled ) {
 				$jobs['end_time'] = time();
 				$jobs['status']   = 'stalled';
 
 				$have_changes = true;
-		}
+			}
 		}
 
 		// If we have changes, update the option in the db.
 		if ( $have_changes ) {
-		$this->save_jobs();
+			$this->save_jobs();
 		}
 	}
 
