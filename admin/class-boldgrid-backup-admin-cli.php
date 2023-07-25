9ef8b6fcdb52610566d21c5a56a083a216a76c89
SitesByYogi <90996605+SitesByYogi@users.noreply.github.com>
2023-07-25 10:38:11 -0400

    Update class-boldgrid-backup-admin-cli.php

--- admin/class-boldgrid-backup-admin-cli.php
+++ admin/class-boldgrid-backup-admin-cli.php
@@ -135,20 +135,16 @@ class Boldgrid_Backup_Admin_Cli {
 
 				case 'popen':
 					$handle = popen( $command, 'r' );
-				
 					// Proc may be seen as available, but our command is not working. Break and try another exec method.
 					if ( false === $handle ) {
-						break;
+					     break;
 					}
 				
 					$output = fread( $handle, 4096 );
-				
 					// Close the process handle and get the return status.
 					$return_var = pclose( $handle );
-				
 					// If the exit status is int(0), then it was successful.
 					$success = 0 === $return_var;
-				
 					break 2;
 				
 				case 'proc_open':
