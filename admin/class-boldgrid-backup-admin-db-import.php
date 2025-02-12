1cf6fee8b02c6c710a7b22671377bd863d105ea3
James Rosado <jamesros@boldgrid.com>
2025-02-12 16:01:16 -0500

    remove fix_definer debug line

--- admin/class-boldgrid-backup-admin-db-import.php
+++ admin/class-boldgrid-backup-admin-db-import.php
@@ -282,7 +282,6 @@ class Boldgrid_Backup_Admin_Db_Import {
 	 * @return string The line with the DEFINER option removed.
 	 */
 	public function fix_definer( $line ) {
-		error_log( 'Fixing definer before: ' . $line );
 		$line_fixed_definer  = '';
 		$sql_security_offset = strpos( $line, 'SQL SECURITY' );
 		$line_fixed_definer  = substr( $line, 0, 9 );
@@ -298,7 +297,6 @@ class Boldgrid_Backup_Admin_Db_Import {
 			$line_fixed_definer .= '*/';
 		}
 
-		error_log( 'Fixing definer after: ' . $line_fixed_definer );
 		return $line_fixed_definer;
 	}
 
